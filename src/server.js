1324ea575bfbfafd64197f004016b0dca56228b2
men1692 <men16922@gmail.com>
2023-02-14 22:46:17 +0900

    학습

--- src/server.js
+++ src/server.js
@@ -1,34 +1,24 @@
 const express = require("express");
 const app = express();
+const { userRouter } = require("./routes/userRoute");
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-const users = [];
+mongoose.set("strictQuery", true);
 
 const MONGO_URI =
   "mongodb+srv://men1692:qudals1004!@atlascluster.x25rhnu.mongodb.net/BlogService?retryWrites=true&w=majority";
 
 const server = async () => {
   try {
-    await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology:true });
-    console.log('mongodb connected');
-
+    await mongoose.connect(MONGO_URI);
+    mongoose.set("debug", true);
+    console.log("mongodb connected");
     app.use(express.json());
 
-    app.get("/user", function (req, res) {
-      return res.send({ users: users });
-    });
+    app.use("/user", userRouter);
 
-    app.post("/user", function (req, res) {
-      console.log(req.body);
-      users.push({ name: req.body.name, age: req.body.age });
-      return res.send({ success: true });
-    });
-
-    app.listen(3000, function () {
-      console.log("server listening on port 30000");
-    });
+    app.listen(3000, () => console.log("server listening on port 3000"));
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 };
 
